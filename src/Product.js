e9649a3e1ff2f71e7e5ada281809b931f08a13da
Harry Dame <enriquedabul@gmail.com>
2020-12-29 01:37:34 -0300

    header and home

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Product.css";
+import StarRateIcon from '@material-ui/icons/StarRate';
+
+function Product({id, title, image, price, rating}) {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p className="product__title">{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i)=>(<StarRateIcon className="star"/>))}
+                </div>
+            </div>
+            <img src={image} alt="product image"/>
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
